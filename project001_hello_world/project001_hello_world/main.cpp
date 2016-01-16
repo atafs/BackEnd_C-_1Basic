31eff46fbc431997efa409b3cd96cbe96312fc60
atafs <atafs@iscte-iul.pt>
2016-01-16 17:54:13 +0000

    project001 in C++ helloworld

---
+++ project001_hello_world/project001_hello_world/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//IMPORTS
+using namespace std;
+
+//MAIN
+int main()
+{
+	//PRINT: CONSOLE OUTPUT (cout)
+	cout << "Hello World of C++";
+
+	//PAUSE
+	cin.get();
+
+	//EXIT
+	return 0;
+}
